2e57b11a99378e1fe98c3b917c255b86b8458826
Erwan Marega <maregaerwan@gmail.com>
2025-09-18 12:50:51 +0200

    Routes last-metro et next_metro

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO stations (name, line, headway_min, last_metro) VALUES
+  ('La Défense', 'M1', 3, '00:40'),
+  ('Champs-Élysées - Clemenceau', 'M1', 3, '00:45'),
+  ('Châtelet', 'M1', 3, '00:55'),
+  ('Nation', 'M1', 3, '01:05');
